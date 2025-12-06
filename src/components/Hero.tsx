1d34277b32c6b81389c09e72e8385ec008dd1a28
Andre Thomas <drethedevjs@gmail.com>
2025-12-06 07:51:02 -0500

    Forced the text on the overlay to be white.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,10 +9,10 @@ const Hero = () => {
       <div className="hero-overlay"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="lg:max-w-5xl md:max-w-xl max-w-md">
-          <h1 className="font-good-times mb-5 lg:text-7xl md:text-5xl font-bold text-3xl text-shadow-2xs">
+          <h1 className="font-good-times mb-5 lg:text-7xl md:text-5xl font-bold text-3xl text-shadow-2xs text-white!">
             Boys of Superior Standard
           </h1>
-          <p className="mb-5 md:text-3xl text-lg">
+          <p className="mb-5 md:text-3xl text-lg text-white!">
             Aspiring to be community leaders, faithful husbands, and loving
             fathers.
           </p>
