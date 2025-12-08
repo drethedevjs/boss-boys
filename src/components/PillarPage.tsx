8accb50723b1467af6c452f811dc9c5e2c6a947d
Andre Thomas <drethedevjs@gmail.com>
2025-12-08 12:25:53 -0500

    Made back button text white.

--- src/components/PillarPage.tsx
+++ src/components/PillarPage.tsx
@@ -13,7 +13,7 @@ export const PillarPage = ({
       <h1 className="page-header mb-10">{pillar}</h1>
       {children}
       <NavLink to={{ pathname: "/", hash: "pillars" }}>
-        <button className="boss-btn bg-boss-green dark:bg-boss-gold">
+        <button className="boss-btn bg-boss-green dark:bg-boss-gold text-white">
           Back
         </button>
       </NavLink>
