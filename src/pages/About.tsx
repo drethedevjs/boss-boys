dee1e1868e3dea12701b7c8abbe81ffa0de8b22f
Andre Thomas <drethedevjs@gmail.com>
2025-12-09 12:46:32 -0500

    Updated About page copy.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,18 +16,19 @@ export const About = () => {
       <section aria-labelledby="scope">
         <h2 id="scope">Scope</h2>
         <p>
-          It is no secret that the centuries of treacherous history experienced
-          by African Americans in the United States have left a legacy that
-          continues to affect Black people—especially Black men. The organizers
-          of B.O.S.S. take this reality to heart and believe that reversing this
-          legacy is not won on Capitol Hill, in statehouses, or solely through
-          school politics.
+          American's past has set African Americans on a rough future. One piece
+          of evidence is abundance of single parent homes in black communities
+          which effects to wholistic development of the children therein. This
+          amplifies the traditional issues that children face such as identity,
+          belonging, and the like. The organizers of B.O.S.S. believe that
+          reversing this legacy is not solely won on Capitol Hill, in
+          statehouses, or through school politics.
         </p>
         <p>
           We believe wholeheartedly that Black men must deny the luxury of
           selfish ambition in order to step into roles as fathers to the
           fatherless, spiritual surgeons who help heal deep wounds, and large
-          stone pillars supporting the often-complicated Black family dynamic.
+          stone pillars supporting the often-complicated black family dynamic.
         </p>
       </section>
 
@@ -35,18 +36,17 @@ export const About = () => {
         <h2 id="what-it-is-not">What It Is Not</h2>
         <p>
           B.O.S.S. is not intended to be a behavior center for "bad kids." We
-          are not looking to focus on students labeled as problematic. That
-          said, our mission does include reaching boys who are acquainted with
-          in-school suspension because some of those students are there
-          precisely because they are missing the consistent, positive support
-          our program provides.
+          are not looking to focus on students labeled as problematic.
+          Nonetheless, they are welcome to participate. Our focus is primarily
+          on boys of color who want to grow and/or those who recognize a void in
+          themselves because of rejection from family members or peers.
         </p>
         <p>
-          While we may organize study halls, B.O.S.S. is not primarily a
-          tutoring program. We believe our boys are capable of meeting—and
-          exceeding—the academic expectations placed on them. When a student
-          struggles academically, we view that often as a symptom of a larger
-          absence of steady, positive Black male leadership in their lives.
+          B.O.S.S. is not a tutoring program. We believe our boys are capable of
+          meeting—and exceeding—the academic expectations placed on them. When a
+          student struggles academically, we tend to view that as a symptom of a
+          larger absence of steady, positive black male leadership in their
+          lives.
         </p>
       </section>
     </div>
