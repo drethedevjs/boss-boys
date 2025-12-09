dca75fd9f4c663342dbcb9a5843d7473098aff5f
Andre Thomas <drethedevjs@gmail.com>
2025-12-09 12:05:05 -0500

    Added visual separation for links in pillars learn more component.

--- src/components/PillarsLearnMore.tsx
+++ src/components/PillarsLearnMore.tsx
@@ -10,16 +10,16 @@ export const PillarsLearnMore = ({
   const leftLink = "/pillar/" + leftText.toLowerCase().replace(" ", "-");
   const rightLink = "/pillar/" + rightText.toLowerCase().replace(" ", "-");
   return (
-    <section className="text-center mb-10">
+    <section className="text-center mb-10 mx-8">
       <h2 className="text-2xl md:text-3xl mb-2">Learn More</h2>
       <p className="mb-4">
         Read about our other two pillars by clicking one of the links below.
       </p>
-      <div className="flex space-x-5 text-2xl font-good-times place-items-center place-content-center">
+      <div className="flex md:text-2xl font-good-times place-items-center place-content-center gap-4">
         <NavLink to={leftLink} className="hover:text-boss-gold">
           {leftText}
-        </NavLink>{" "}
-        | &nbsp;
+        </NavLink>
+        <div className="w-1 h-8 bg-boss-gold"></div>
         <NavLink to={rightLink} className="hover:text-boss-gold">
           {rightText}
         </NavLink>
