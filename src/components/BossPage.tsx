fda413d12c53d7203e594bd59772de2f0b24fff3
Andre Thomas <drethedevjs@gmail.com>
2025-12-10 13:12:17 -0500

    Made header optional.

--- src/components/BossPage.tsx
+++ src/components/BossPage.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 export const BossPage = ({
-  header,
+  header = "",
   children
 }: {
-  header: string;
+  header?: string;
   children: React.ReactNode;
 }) => {
   return (
     <div className="boss-container">
-      <h1 className="page-header">{header}</h1>
+      {header && <h1 className="page-header">{header}</h1>}
       {children}
     </div>
   );
