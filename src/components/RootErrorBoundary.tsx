1fafb91a7a3f9877dcf4e8fbd159aaa1b8d3cf5e
Andre Thomas <drethedevjs@gmail.com>
2025-12-13 09:59:31 -0500

    Styled the error page.

--- src/components/RootErrorBoundary.tsx
+++ src/components/RootErrorBoundary.tsx
@@ -1,14 +1,25 @@
-import { isRouteErrorResponse, useRouteError } from "react-router";
+import { isRouteErrorResponse, NavLink, useRouteError } from "react-router";
+import { Navbar } from "./Navbar";
 
 export const RootErrorBoundary = () => {
   let error = useRouteError();
   if (isRouteErrorResponse(error)) {
     return (
       <>
-        <h1>
-          {error.status} {error.statusText}
-        </h1>
-        <p>{error.data}</p>
+        <Navbar />
+        <div className="boss-container grid place-items-center text-center min-h-[calc(100vh-var(--nav-height))]">
+          <div>
+            <h1 className="lg:text-9xl text-6xl font-bold">{error.status}</h1>
+            <h2>{error.statusText}</h2>
+            <p>{error.data}</p>
+            <NavLink
+              to="/"
+              className="boss-btn text-white ring-1 ring-boss-green bg-boss-green hover:bg-white hover:text-boss-green dark:ring-boss-gold dark:bg-boss-gold dark:hover:bg-transparent dark:hover:text-boss-gold"
+            >
+              Go Back Home
+            </NavLink>
+          </div>
+        </div>
       </>
     );
   } else if (error instanceof Error) {
